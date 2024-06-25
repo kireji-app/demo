4eda51102f675d312d121b60df83f8986de654ea
adminowicz <contact@ejaugust.com>
2024-06-25 15:00:02 -0400

    version 39 rearrange flow, move testing sequence directly into the root, preparing for tests themselves to be content files.

--- live.js
+++ live.js
@@ -1,191 +1,175 @@
-const
- debug_limit = 1000,
- root = {
-  ["version.float"]: {
-   value: 38 / 1000
-  },
-  ["name.txt"]: {
-   get() { return "https://core.parts" }
-  },
-  ["prototype.branch"]: {
-   get() { return Object.getPrototypeOf(this) }
-  },
-  [".branches"]: {
-   value: {}
-  },
-  ["view.node"]: {
-   get() { return document.body }
-  },
-  ["shadow.node"]: {
-   get() { return this["#shadow"] ??= this["view.node"].attachShadow({ mode: "closed" }) }
-  },
-  ["path.uri"]: {
-   get() { return (this["prototype.branch"]?.["path.uri"] ?? '') + this["name.txt"] + '/' }
-  },
-  ["view.html"]: {
-   get() { return `I am ${this["live.color"]}.` }
-  },
-  ["layout.css"]: {
-   get() { return `:host { background: ${this["live.color"]} }` }
-  },
-  ["view.stylesheet"]: {
-   get() {
-    if (!this["#stylesheet"]) {
-     this["#stylesheet"] = new CSSStyleSheet()
-     this["shadow.node"].adoptedStyleSheets.push(this["#stylesheet"])
+onload = e => {
+ const
+  root = {
+   ".branches": { value: {} },
+   "name.txt": { value: "https://core.parts" },
+   "live.color": { value: "tomato" },
+   "green.color": { value: "green" },
+   "brown.color": { value: "brown" },
+   "yellow.color": { value: "yellow" },
+   "version.float": { value: 39 / 1000 },
+   "brown.map": { value: [["green.color", "brown.color"]] },
+   "yellow.map": { value: [["green.color", "yellow.color"]] },
+   "dev.core.parts.map": { value: [["live.color", "green.color"]] },
+   "root.map": {
+    get() {
+     return root
     }
-    return this["#stylesheet"]
-   }
-  },
-
-  ["live.color"]: {
-   value: 'tomato'
-  },
-  ["green.color"]: {
-   value: 'green'
-  },
-  ["yellow.color"]: {
-   value: 'yellow'
-  },
-  ["brown.color"]: {
-   value: 'brown'
-  },
-
-  ["root.map"]: {
-   get() { return root }
-  },
-  ["dev.core.parts.map"]: {
-   value: [["live.color", "green.color"]]
-  },
-  ["brown.map"]: {
-   value: [["green.color", "brown.color"]]
-  },
-  ["yellow.map"]: {
-   value: [["green.color", "yellow.color"]]
-  },
-
-  ["append.run"]: {
-   get() {
-    return name => {
-
-     const descriptorMap = this["getDescriptorMap.run"](name);
-
-     return this[".branches"][name] = Object.create(this, {
-      ...descriptorMap,
-      ["name.txt"]: { value: name },
-      ["root.map"]: { value: descriptorMap },
-      [".branches"]: { value: {} }
-     })
-    }
-   }
-  },
-  ["donate.run"]: {
-   get() {
-    return (branch, recipient, name = branch["name.txt"]) => {
-     delete this[".branches"][name]
-     recipient[".branches"][name] = branch
-     Object.setPrototypeOf(branch, recipient)
-     branch["render.run"]()
-    }
-   }
-  },
-  ["insert.run"]: {
-   get() {
-    return name => {
-
-     const insertedBranch = this["append.run"](name)
-
-     for (const subname in this[".branches"]) {
-      const branch = this[".branches"][subname];
-      if (branch === insertedBranch) continue
-      this["donate.run"](branch, insertedBranch, subname)
+   },
+   "view.node": {
+    get() {
+     return document.body;
+    },
+   },
+   "prototype.branch": {
+    get() {
+     return Object.getPrototypeOf(this);
+    },
+   },
+   "shadow.node": {
+    get() {
+     return (this["#shadow"] ??= this["view.node"].attachShadow({
+      mode: "closed",
+     }));
+    },
+   },
+   "path.uri": {
+    get() {
+     return (
+      (this["prototype.branch"]?.["path.uri"] ?? "") + this["name.txt"] + "/"
+     );
+    },
+   },
+   "view.html": {
+    get() {
+     return `<h1><b>I am <i>${this["live.color"]}.</i></b></h1>`;
+    },
+   },
+   "layout.css": {
+    get() {
+     return `:host { background: ${this["live.color"]} }`;
+    },
+   },
+   "view.stylesheet": {
+    get() {
+     return (
+      this["#stylesheet"] ||
+      ((this["#stylesheet"] = new CSSStyleSheet()),
+       this["shadow.node"].adoptedStyleSheets.push(this["#stylesheet"])),
+      this["#stylesheet"]
+     );
+    },
+   },
+   "branch.run": {
+    get() {
+     return t => {
+      let r = this["getDescriptorMap.run"](t);
+      console.log('branching ' + this["path.uri"] + ' + ' + t)
+      return (this[".branches"][t] = Object.create(this, {
+       ...r,
+       "name.txt": { value: t },
+       "root.map": { value: r },
+       ".branches": { value: {} },
+      }));
+     };
+    },
+   },
+   "donate.run": {
+    get() {
+     return (t, r, o = t["name.txt"]) => {
+      let oldpath = t["path.uri"]
+      delete this[".branches"][o]
+      r[".branches"][o] = t
+      Object.setPrototypeOf(t, r)
+      t["render.run"]()
+      console.log('moved ' + oldpath + ' to ' + t["path.uri"])
+     };
+    },
+   },
+   "insert.run": {
+    get() {
+     return t => {
+      let r = this["branch.run"](t);
+      for (let o in this[".branches"]) {
+       let n = this[".branches"][o];
+       n !== r && this["donate.run"](n, r, o);
+      }
+      return r;
+     };
+    },
+   },
+   "remove.run": {
+    get() {
+     return () => {
+      if (this === core)
+       throw Error("Error: Attempted to delete core.");
+      let t = this["prototype.branch"], r = this[".branches"];
+      delete t[".branches"][this["name.txt"]]
+      for (let o in r) this["donate.run"](r[o], t, o)
+      return t;
+     };
+    },
+   },
+   "replaceWith.run": {
+    get() {
+     return t => {
+      if (this === core)
+       return this["insert.run"](t);
+      let r = this["prototype.branch"]["branch.run"](t),
+       o = this[".branches"];
+      for (let n in o) this["donate.run"](o[n], r, n);
+      return r;
+     };
+    },
+   },
+   "getDescriptorMap.run": {
+    get() {
+     return t => {
+      if (t === core["name.txt"]) return root;
+      if (!(t + ".map" in this))
+       throw RangeError('No such map as "' + t + '.map"');
+      let r = {};
+      for (let [o, n] of this[t + ".map"])
+       r[o] = {
+        get() {
+         return this["prototype.branch"][n];
+        },
+       };
+      return r;
+     };
+    },
+   },
+   "render.run": {
+    get() {
+     return () => {
+      console.log('render called on ' + this["path.uri"])
+      this["view.stylesheet"].replaceSync(this["layout.css"]),
+       (this["shadow.node"].innerHTML = this["view.html"]),
+       this["view.node"].setAttribute("data-path", this["path.uri"]);
+     };
+    },
+   },
+   "test.run": {
+    get() {
+     return () => {
+      requestAnimationFrame(() => {
+       const brown = this["insert.run"]("brown");
+       requestAnimationFrame(() => {
+        const yellow = brown["replaceWith.run"]("yellow")
+        requestAnimationFrame(() => {
+         yellow["remove.run"]();
+         requestAnimationFrame(() => {
+          console.log(this)
+          //this["test.run"]()
+         })
+        })
+       })
+      })
      }
-
-     return insertedBranch
     }
    }
   },
-  ["remove.run"]: {
-   get() {
-    return () => {
-
-     if (this["path.uri"] === root["name.txt"].get())
-      throw Error("Error: Attempted to delete core node.")
-
-     const
-      prototype = this["prototype.branch"],
-      branches = this[".branches"];
-
-     for (const subname in branches)
-      this["donate.run"](branches[subname], prototype, subname)
-
-     return prototype
-    }
-   }
-  },
-  ["replaceWith.run"]: {
-   get() {
-    return name => {
-
-     if (this["path.uri"] === root["name.txt"].get())
-      return this["insert.run"](name)
-
-     const
-      newCore = this["prototype.branch"]["append.run"](name),
-      branches = this[".branches"];
-
-     for (const subname in branches)
-      this["donate.run"](branches[subname], newCore, subname)
-
-     return newCore
-    }
-   }
-  },
-  ["getDescriptorMap.run"]: {
-   get() {
-    return name => {
-
-     if (debug++ >= debug_limit) throw 'over limit'
-
-     if (name === root["name.txt"].get())
-      return root
-
-     if (!(name + '.map' in this))
-      throw RangeError('No such map as "' + name + '.map"')
-
-     const map = {}
-
-     for (const [a, b] of this[name + '.map'])
-      map[a] = { get() { return this["prototype.branch"][b] } }
-
-     return map
-    }
-   }
-  },
-  ["render.run"]: {
-   get() {
-    return () => {
-     // TODO: Spawn fx network instead of full rerender.
-     // TODO: Turn view.html and view.stylesheet into ordered lists of nodes and stylesheets, respectively.
-     this["view.stylesheet"].replaceSync(this["layout.css"])
-     this["shadow.node"].innerHTML = this["view.html"]
-     this["view.node"].setAttribute('data-path', this["path.uri"])
-    }
-   }
-  },
- }, hosted = Object.create(null, root)["append.run"](location.host)
-
-var debug = 0
-onload = () => {
- // Tests...
- hosted["render.run"]()
- setTimeout(() => {
-  const browned = hosted["prototype.branch"]["insert.run"]("brown")
-  setTimeout(() => {
-   const yellowed = browned["replaceWith.run"]("yellow")
-   setTimeout(() => {
-    yellowed["remove.run"]()
-   }, 3000)
-  }, 3000)
- }, 2000)
+  core = Object.create(null, root);
+ core["branch.run"](location.host)["render.run"]();
+ core["test.run"]()
 }
