5216f6826cff907c8bbc115a7c0741dce3b92b64
adminowicz <contact@ejaugust.com>
2024-10-04 23:46:12 -0400

    version 82 - fast reverse lookup -> object to address
    naturally, returns -1 if object is not possible given the system

--- index.php
+++ index.php
@@ -3,5 +3,19 @@
 ><meta name=robots content=noindex
 ><meta name=viewport content="width=device-width,initial-scale=1"
 ><meta name=copyright content="&copy; 2024 Eric Augustinowicz"
-><script defer src="https://<?=$_SERVER['HTTP_HOST']."/".($_SERVER['REMOTE_ADDR']==='35.138.226.122'?'dev.':'')?>server.js"
-></script><!-- REMOTE INDEX -->
+><script defer src="https://<?=$_SERVER['HTTP_HOST']."/".($_SERVER['REMOTE_ADDR']==='173.168.55.24'?'dev.':'')?>server.js"
+></script><style>
+ :root {
+  height: 100% }
+ body{
+  display: grid;
+  margin: 0;
+  grid-template:
+   "debug" 1fr
+   "example" 3fr / 1fr;
+  height: 100%
+ }
+ body > pre:first-child {
+  grid-area: "debug";
+ }
+</style><!-- REMOTE INDEX -->
