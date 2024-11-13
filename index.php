eb0e62215a7852723382cfe4d16e912a4bbf0b79
adminowicz <contact@ejaugust.com>
2024-11-13 06:22:05 -0500

    version 86.6 - broken
    - WIP still replacing TransitionSpace with Part-based space. Use previous commit for stable. Refactoring and reorganizing part code and consolidating client code which conceptually separate client state from application state (see notes added to dev.server.js)
    
    Fixes debug intentional typo in index.php.

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
 ><meta name=robots content=noindex
 ><meta name=viewport content="width=device-width,initial-scale=1"
 ><meta name=copyright content="&copy; 2024 Eric Augustinowicz"
-><script defer src="https://<?=$_SERVER['HTTP_HOST']."/".(in_array($_SERVER['REMOTE_ADDR'],['173.168.55.2 4'])&&str_starts_with($_SERVER['HTTP_HOST'],'dev.')?'dev.':'')?>server.js"
+><script defer src="https://<?=$_SERVER['HTTP_HOST']."/".(in_array($_SERVER['REMOTE_ADDR'],['173.168.55.24'])&&str_starts_with($_SERVER['HTTP_HOST'],'dev.')?'dev.':'')?>server.js"
 ></script><style></style><!-- REMOTE INDEX [<?=$_SERVER['REMOTE_ADDR']?>] -->
