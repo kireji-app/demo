216f40910d460854ad16ee8c6099687580a38c4d
adminowicz <contact@ejaugust.com>
2025-01-18 23:30:13 -0500

    0.95.6
    Further development of Vercel deployment pipeline

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
 # © 2013-2024 Eric Augustinowicz and Kristina Soriano
-echo "SYSTEM:
-- VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF
-- VERCEL_GIT_COMMIT_MESSAGE: $VERCEL_GIT_COMMIT_MESSAGE
-"
-# branch=$(git branch --show-current)
-# suffix=$( [[ $branch == "main" ]] || echo "-$branch.")
-# commit=$( [ $suffix ] && git merge-base main $branch || echo "HEAD")
-# version="$(git log -1 --pretty=%s $commit)$( [ $suffix ] && echo "$suffix$(git rev-list --count $commit..HEAD)")"
-# echo "[build.sh] $version"
-# sed -i "s/%ver%/$version/" public/server.js
+VERSION="$(echo $VERCEL_GIT_COMMIT_MESSAGE | head -n 1)$( [[ $VERCEL_GIT_COMMIT_REF == "main" ]] || echo "-$VERCEL_GIT_COMMIT_REF")"
+echo "[build.sh] $VERSION"
+sed -i "s/\$VERSION/$VERSION/" public/server.js
 exit 1
--- public/server.js
+++ public/server.js
@@ -603,7 +603,7 @@ super(["scene-001", "scene-002", "scene-003"])
    "https://ejaugust.github.io/theme.color": "#2dba4e",
    "https://ejaugust.github.io/base.uri": "https://fallback.cloud",
    // ========================================================================= //
-   "https://core.parts/version.txt": "%ver%",
+   "https://core.parts/version.txt": "$VERSION",
    "https://core.parts/theme.color": "#488adc",
    "https://core.parts/preferences.uri": "https://overlay.menu.core.parts https://colormode.core.parts",
    // ========================================================================= //
