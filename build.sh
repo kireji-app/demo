ce0fcf0bcd4fcda0de3452ca43251c615d125071
adminowicz <contact@ejaugust.com>
2025-01-18 23:17:30 -0500

    0.95.5
    Debug commit - test Varcel system variables.

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 # © 2013-2024 Eric Augustinowicz and Kristina Soriano
-branch=$(git branch --show-current)
-suffix=$( [[ $branch == "main" ]] || echo "-$branch.")
-commit=$( [ $suffix ] && git merge-base main $branch || echo "HEAD")
-version="$(git log -1 --pretty=%s $commit)$( [ $suffix ] && echo "$suffix$(git rev-list --count $commit..HEAD)")"
-echo "[build.sh] $version"
-sed -i "s/%ver%/$version/" public/server.js
+echo "SYSTEM:
+- VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF
+- VERCEL_GIT_COMMIT_MESSAGE: $VERCEL_GIT_COMMIT_MESSAGE
+"
+# branch=$(git branch --show-current)
+# suffix=$( [[ $branch == "main" ]] || echo "-$branch.")
+# commit=$( [ $suffix ] && git merge-base main $branch || echo "HEAD")
+# version="$(git log -1 --pretty=%s $commit)$( [ $suffix ] && echo "$suffix$(git rev-list --count $commit..HEAD)")"
+# echo "[build.sh] $version"
+# sed -i "s/%ver%/$version/" public/server.js
 exit 1
