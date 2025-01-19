fb685b9c7355215db526ae999d1a549e67876a3b
adminowicz <contact@ejaugust.com>
2025-01-18 23:00:47 -0500

    0.95.3
    Fix vercel bug

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # © 2013-2024 Eric Augustinowicz and Kristina Soriano
 branch=$(git branch --show-current)
 suffix=$( [[ $branch == "main" ]] || echo "-$branch.")
-commit=$( [ $suffix ] && git merge-base main test-feature || echo "HEAD")
+commit=$( [ $suffix ] && git merge-base main $branch || echo "HEAD")
 version="$(git log -1 --pretty=%s $commit)$( [ $suffix ] && echo "$suffix$(git rev-list --count $commit..HEAD)")"
 echo "[build.sh] $version"
 sed -i "s/%ver%/$version/" public/server.js
