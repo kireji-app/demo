cca96e2ce2657536c70ca90b6973f585c66cd1dc
adminowicz <contact@ejaugust.com>
2025-04-09 14:07:56 -0400

    0.111.0
    Try to fix serverless function only working in local environment.

--- dns-root/parts/desktop/build/initialize.js
+++ dns-root/parts/desktop/build/initialize.js
@@ -9,10 +9,5 @@ const {
 if (!itemExists("api"))
  makeFolder("api")
 
-// Write the output files.
-if (!itemExists("public"))
- makeFolder("public")
-
 writeFile("api/serverless.js", desktop["serverless.js"])
-writeFile("public/serverless.js", desktop["serverless.js"])
 writeFile("README.md", readFile("README.md", "utf-8").replace(/version-\d+\.\d+\.\d+/, "version-" + BUILD_TAGS[0]))
