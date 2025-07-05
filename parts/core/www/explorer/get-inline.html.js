a978e2d85bff850614134b5ab7a4bf24d75bc428
adminowicz <contact@ejaugust.com>
2025-07-05 05:01:44 -0400

    0.117.4
    Rename a couple of apps.

--- parts/core/www/explorer/get-inline.html.js
+++ parts/core/www/explorer/get-inline.html.js
@@ -1,6 +1,6 @@
 return (
  `<part-explorer>` + (
-  "<h2>Schema Explorer</h2>" +
+  "<h2>Part Explorer</h2>" +
   explorer.recursiveItemHTML([_], 0) +
   width["inline.html"]
  ) + "</part-explorer>"
