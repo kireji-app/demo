af1f09e034aeb92ed4187bc37ea5a06a43797eb5
adminowicz <contact@ejaugust.com>
2025-07-12 04:46:43 -0400

    0.117.16
    Remove extra log

--- parts/desktop/server/sync-install.js
+++ parts/desktop/server/sync-install.js
@@ -32,14 +32,6 @@ require('http').createServer((request, response) => {
  let status, head, body
 
  if (request.url === "/kireji.js") {
-  debug({
-   'if-match': request.headers['if-match'],
-   'if-modified-since': request.headers['if-modified-since'],
-   'if-range': request.headers['if-range'],
-   'if-none-modified-since': request.headers['if-none-modified-since'],
-   'etag': request.headers.etag,
-   'server ETag': ETag
-  })
   if (request.headers['if-none-match'] === ETag) {
    status = 304
    head = { ETag }
