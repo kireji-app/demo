3595e17945f9f61e9c7e971324429aff0f56c9fc
adminowicz <contact@ejaugust.com>
2025-11-04 11:53:16 -0500

    0.133.5
    Remove three unused parts: `range.core.parts`, `rectangle.core.parts` and `slot.core.parts`.

--- src/parts/core/range/build.js
+++
@@ -1,9 +0,0 @@
-const max = BigInt(part.max)
-const min = BigInt(part.min)
-
-if (max < min)
- throw new RangeError(`Invalid range (min: ${min}, max: ${max}) (on ${part.host}).`)
-
-Object.defineProperties(part, {
- cardinality: { value: (max - min) + 1n, configurable: true, writable: true }
-})
--- src/parts/core/slot/build.js
+++
@@ -1,5 +0,0 @@
-const slotManifest = part["slot.json"]
-
-if (!slotManifest)
- throw new ReferenceError(`Slot missing slot.json manifest (${part.host}).`)
-
